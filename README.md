7ebef0eca091cef863ebe2c01aa118565ed15cd6
Lavanya2099 <lavanyayc7@gmail.com>
2021-10-06 08:36:24 +0530

    add vue cart

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# lazyloading
+# shoppingcart
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <button v-on:click="navigateTo('products')">View products</button>
+      {{ cart.length }} in cart
+      <button v-on:click="navigateTo('cart')">View Cart</button>
+    </header>
+
+    <div v-if="page === 'cart'">
+      <Cart v-on:removeItemFromCart="removeItemFromCart" :cart="cart" />
+    </div>
+
+    <div v-if="page === 'products'">
+      <Products v-on:addItemToCart="addItemToCart" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Products from "./components/Products.vue";
+import Cart from "./components/Cart.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => {
+    return {
+      page: "products",
+      cart: [],
+    };
+  },
+  methods: {
+    
+    
+    addItemToCart(product) {
+      this.cart.push(product);
+    },
+    removeItemFromCart(product) {
+      this.cart.splice(this.cart.indexOf(product), 1);
+    },
+    navigateTo(page) {
+      this.page = page;
+    },
+  },
+  components: { Products, Cart },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  background-color: rgb(252, 110, 66);
+}
+.products {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  margin-left: 20px;
+  justify-content: center;
+}
+.products button {
+  padding: 10px;
+  background-color: black;
+  color: rgb(204, 74, 14);
+  outline: none;
+  border: none;
+  cursor: pointer;
+}
+.products button:hover {
+  color: peru;
+}
+</style>
+
+<style scoped>
+header {
+  height: 100px;
+  box-shadow: 2px 2px 5px rgb(223, 74, 48);
+  background-color: white;
+  text-align: right;
+  font-size: 30px;
+  padding-top: 20px;
+}
+header button {
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+  color: white;
+  background-color: brown;
+  box-shadow: 2px 2px 5px rgb(223, 74, 48);
+}
+header button:hover {
+  color: rgb(204, 90, 14);
 }
 </style>
